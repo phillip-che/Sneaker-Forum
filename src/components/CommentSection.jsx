5c4cd1b90808b2187f3241d36a5de262fc05e59d
phillip-che <pche@cpp.edu>
2023-04-14 12:55:16 -0700

    Fixed rendering of comments after user commenting

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -2,10 +2,22 @@ import Comment from "./Comment";
 import { supabase } from "../client";
 import { useState, useEffect } from "react";
 
-const CommentSection = ({ comments, postID, timeSince }) => {
+const CommentSection = ({ postID, timeSince }) => {
 
     const [commentInput, setCommentInput] = useState("");
     const [user, setUser] = useState("");
+    const [comments, setComments] = useState(null);
+
+    const getComments = async () => {
+        await supabase
+          .from("Comments")
+          .select()
+          .eq("post_id", postID)
+          .then((response) => {
+            console.log(response.data);
+            setComments(response.data);
+          });
+      };
 
     useEffect(() => {
         supabase.auth.onAuthStateChange((event, session) => {
@@ -14,18 +26,7 @@ const CommentSection = ({ comments, postID, timeSince }) => {
         }
         });
 
-        // const getComments = async () => {
-        //     await supabase
-        //       .from("Comments")
-        //       .select()
-        //       .eq("post_id", params.postID)
-        //       .then((response) => {
-        //         console.log(response.data);
-        //         setComments(response.data);
-        //       });
-        //   };
-        //   getComments();
-
+          getComments();
     }, []);
 
     const handleChange = (event) => {
@@ -38,32 +39,40 @@ const CommentSection = ({ comments, postID, timeSince }) => {
         .from("Comments")
         .insert({'post_id' : postID, 'author' : user, 'comment' : commentInput})
         .select()
+        .then((response) => {
+            console.log(response);
+            getComments();
+        })
     };
 
     return (
         <div className="comment-container">
     
-                <textarea
-                className="comment-content"
-                placeholder="Leave a comment"
-                name="comment"
-                onChange={handleChange}
-                ></textarea>
-                <button className="comment-button" onClick={addComment} >Comment</button>
-                
+            <textarea
+            className="comment-content"
+            placeholder="Leave a comment"
+            name="comment"
+            onChange={handleChange}
+            ></textarea>
+            <button disabled={commentInput.length < 1} className="comment-button" onClick={addComment} >Comment</button>
+            
+            {comments ? (
                 <div className="comment-list">
-                    <h3>Comments</h3>
-                    <hr class="gradient" />
-                    {comments
-                    ? comments.map((comment) => (
-                        <Comment
-                            author={comment.author}
-                            comment={comment.comment}
-                            date={timeSince(new Date(comment.created_at))}
-                        />
-                        ))
-                    : null}
+                <h3>Comments</h3>
+                <hr class="gradient" />
+                {comments
+                ? comments.map((comment) => (
+                    <Comment
+                        author={comment.author}
+                        comment={comment.comment}
+                        date={timeSince(new Date(comment.created_at))}
+                    />
+                    ))
+                : null}
             </div>
+            ) : null }
+            
+
         </div>
     );
 };

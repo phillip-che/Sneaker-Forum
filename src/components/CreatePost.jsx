d633e08127e0dc255300f2f1996011407c956128
phillip-che <pche@cpp.edu>
2023-04-17 00:25:21 -0700

    Added button to upload images on creating post

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,25 @@
 import { useState, useEffect } from "react"
 import { supabase } from "../client";
+import { v4 as uuid4 } from "uuid";
 
 const CreatePost = () => {
 
     const [user, setUser] = useState("");
     const [input, setInputs] = useState({
+        postID: uuid4(),
         title: "",
         description: "",
+        images: []
     });
 
     useEffect(() => {
+        console.log(input.postID);
         supabase.auth.onAuthStateChange((event, session) => {
+            console.log(session.user);
           if(session) {
-            setUser(session.user.email);
+            setUser(session.user);
           }
-        })
+        });
       }, []);
 
     const handleChange = (event) => {
@@ -30,12 +35,18 @@ const CreatePost = () => {
     const createPost = async () => {
         await supabase
         .from('Posts')
-        .insert([{author: user, title: input.title, description: input.description}])
+        .insert([{id: input.postID, author: user.email, title: input.title, description: input.description}])
         .select()
         .then((data) => {
             console.log(data);
             window.location = "/";
         });
+    }
+
+    const onUpload = (event) => {
+        let file = event.target.files[0];
+        console.log(file);
+        input.images.push(file);
     }
 
     return (
@@ -51,6 +62,16 @@ const CreatePost = () => {
                 </textarea>
             </div>
 
+            {input.images ? (
+                <div>
+                    {input.images.map((image) => {
+                        <img className="upload-image" src={image} />
+                    })}
+                </div>
+            ) : null }
+
+            <input className="image-url" type="file" onChange={(e) => onUpload(e)} />
+            
             <button disabled={input.title.length < 1} className="button" onClick={createPost}>
                 Post
             </button>
